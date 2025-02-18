a083297ae1f3f2a6028d25b6f69d71d7bb0172a6
tianat11 <tianat992@gmail.com>
2025-02-18 11:35:49 -0700

    added week 13

---
+++ JavaScript/ClassWork/WEEK-13/Day1/extinction-event/main.js
@@ -0,0 +1,16 @@
+//select elements
+
+
+//use a for of loop to add a line-through the text a user clicks on
+
+
+
+//use a for of to erase and item when a user clicks on it
+
+
+
+//use a for to make the image of the dinosaur disapear when clicked on 
+
+
+//make all of the dinosaurs disapear when a user clicks the destroy-all element
+//you will need to use document.getElementById() and addEventListener()
