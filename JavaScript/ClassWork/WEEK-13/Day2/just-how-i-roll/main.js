2c3559ce7cbb9959ca640aeec759256cfeacbe04
tianat11 <tianat992@gmail.com>
2025-02-20 14:45:45 -0700

    rolldice activity

--- JavaScript/ClassWork/WEEK-13/Day2/just-how-i-roll/main.js
+++ JavaScript/ClassWork/WEEK-13/Day2/just-how-i-roll/main.js
@@ -29,16 +29,114 @@ const sortByNumber = function(arr) {
 /*******************
  * YOUR CODE BELOW *
  *******************/
+const d6 = document.querySelector('#d6-roll');
+const doubleD6 = document.querySelectorAll('.double.d6.roll');
+const d12 = document.querySelector('#d12-roll');
+const d20 = document.querySelector('#d20-roll');
 
 
 
 /*******************
  * EVENT LISTENERS *
  *******************/
+d6.addEventListener('click',function(){
+  let counter = 0;
+  let random;
+  const num = setInterval(() => {
+  random = getRandomNumber(6); 
+  d6.src = `./images/d6/${random}.png`;
+  counter++
+  if(counter >= 10){
+    clearInterval(num)
+    sixes.push(random)
+    document.getElementById('d6-rolls-mean').innerText = findMean(sixes)
+    document.getElementById('d6-rolls-median').innerText = findMedian(sixes)
+    document.getElementById('d6-rolls-mode').innerText = findMode(sixes)
+  }
+  },30)
+})
+
+const roll6s = () => {
+  let roll1 = getRandomNumber(6);
+  let roll2 = getRandomNumber(6);
+  let roll = roll1 + roll2;
+  doubleSixes.push(roll)
+  document.querySelector("#double-d6-rolls-mean").innerText = findMean(doubleSixes)
+  document.querySelector("#double-d6-rolls-mode").innerText = findMode(doubleSixes)
+  document.querySelector("#double-d6-rolls-median").innerText = findMedian(doubleSixes)
+  doubleD6[0].src = `./images/d6/${roll1}.png`
+  doubleD6[1].src = `./images/d6/${roll2}.png`
+}
+
+doubleD6.forEach((die)=>{
+  die.addEventListener('click', roll6s)
+})
+
+d12.addEventListener('click', () =>{
+  const random = getRandomNumber(12)
+  d12.src = `./images/numbers/${random}.png`
+  twelves.push(random)
+  document.querySelector("#d12-rolls-mean").innerText = findMean(twelves)
+  document.querySelector("#d12-rolls-median").innerText = findMedian(twelves)
+  document.querySelector("#d12-rolls-mode").innerText = findMode(twelves)
+  
+} )
+
+d20.addEventListener('click', () =>{
+  const random = getRandomNumber(20)
+  d20.src = `./images/numbers/${random}.png`
+  twenties.push(random)
+  document.querySelector("#d20-rolls-mean").innerText = findMean(twenties)
+  document.querySelector("#d20-rolls-median").innerText = findMedian(twenties)
+  document.querySelector("#d20-rolls-mode").innerText = findMode(twenties)
+  
+} )
+
+
 
 /******************
  * RESET FUNCTION *
  ******************/
+document.querySelector('#reset-button').addEventListener('click', reset)
+
+function reset(){
+  console.log('called')
+  d6.src = `./images/start/d6.png`
+  doubleD6.forEach((die)=> die.src = `./images/start/d6.png` )
+  d12.src = `./images/start/d12.png`
+  d20.src = `./images/start/d20.png`
+
+  while(sixes.length > 0){
+    sixes.pop()
+  
+  }
+  while(doubleSixes.length > 0){
+    doubleSixes.pop()
+  
+  }
+  while(twelves.length > 0){
+    twelves.pop()
+  
+  }
+  while(twenties.length > 0){
+    twenties.pop()
+  
+  }
+
+ document.querySelector('#d6-rolls-mean').innerText = 'NA'
+ document.querySelector('#d6-rolls-median').innerText = 'NA'
+ document.querySelector('#d6-rolls-mode').innerText = 'NA'
+ document.querySelector('#double-d6-rolls-mean').innerText = 'NA'
+ document.querySelector('#double-d6-rolls-meadian').innerText = 'NA'
+ document.querySelector('#double-d6-rolls-mode').innerText = 'NA'
+ document.querySelector('#d12-rolls-mean').innerText = 'NA'
+ document.querySelector('#d12-rolls-median').innerText = 'NA'
+ document.querySelector('#d12-rolls-mode').innerText = 'NA'
+ document.querySelector('#d20-rolls-mean').innerText = 'NA'
+ document.querySelector('#d20-rolls-median').innerText = 'NA'
+ document.querySelector('#d20-rolls-mode').innerText = 'NA'
+
+}
 
 
 
@@ -51,3 +149,4 @@ const sortByNumber = function(arr) {
 /****************
  * MATH SECTION *
  ****************/
+
