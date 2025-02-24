d0c61702cf438f475007c92ab310aecaca296f03
tianat11 <tianat992@gmail.com>
2025-02-24 14:42:04 -0700

    compare changes in how we roll

--- JavaScript/ClassWork/WEEK-13/Day2/just-how-i-roll/main.js
+++ JavaScript/ClassWork/WEEK-13/Day2/just-how-i-roll/main.js
@@ -1,3 +1,201 @@
+// /**********
+//  * DATA *
+//  **********/
+
+// const sixes = [];
+// const doubleSixes = [];
+// const twelves = [];
+// const twenties = [];
+
+// /********************
+//  * HELPER FUNCTIONS *
+// ********************/
+
+// const getRandomNumber = function(max) {
+//     const rand = Math.random();
+//     const range = rand * max;
+//     const result = Math.ceil(range);
+//     return result;
+// }
+
+// const sortByNumber = function(arr) {
+//   const byNumber = function(item1, item2) {
+//     return item1 - item2;
+//   }
+
+//   return arr.slice().sort(byNumber);
+// }
+
+// /*******************
+//  * YOUR CODE BELOW *
+//  *******************/
+// const d6 = document.querySelector('#d6-roll');
+// const doubleD6 = document.querySelectorAll('.double.d6.roll');
+// const d12 = document.querySelector('#d12-roll');
+// const d20 = document.querySelector('#d20-roll');
+
+
+
+// /*******************
+//  * EVENT LISTENERS *
+//  *******************/
+// d6.addEventListener('click',function(){
+//   let counter = 0;
+//   let random;
+//   const num = setInterval(() => {
+//   random = getRandomNumber(6); 
+//   d6.src = `./images/d6/${random}.png`;
+//   counter++
+//   if(counter >= 10){
+//     clearInterval(num)
+//     sixes.push(random)
+//     document.getElementById('d6-rolls-mean').innerText = findMean(sixes)
+//     document.getElementById('d6-rolls-median').innerText = findMedian(sixes)
+//     document.getElementById('d6-rolls-mode').innerText = findMode(sixes)
+//   }
+//   },30)
+// })
+
+// const roll6s = () => {
+//   let roll1 = getRandomNumber(6);
+//   let roll2 = getRandomNumber(6);
+//   let roll = roll1 + roll2;
+//   doubleSixes.push(roll)
+//   document.querySelector("#double-d6-rolls-mean").innerText = findMean(doubleSixes)
+//   document.querySelector("#double-d6-rolls-mode").innerText = findMode(doubleSixes)
+//   document.querySelector("#double-d6-rolls-median").innerText = findMedian(doubleSixes)
+//   doubleD6[0].src = `./images/d6/${roll1}.png`
+//   doubleD6[1].src = `./images/d6/${roll2}.png`
+// }
+
+// doubleD6.forEach((die)=>{
+//   die.addEventListener('click', roll6s)
+// })
+
+// d12.addEventListener('click', () =>{
+//   const random = getRandomNumber(12)
+//   d12.src = `./images/numbers/${random}.png`
+//   twelves.push(random)
+//   document.querySelector("#d12-rolls-mean").innerText = findMean(twelves)
+//   document.querySelector("#d12-rolls-median").innerText = findMedian(twelves)
+//   document.querySelector("#d12-rolls-mode").innerText = findMode(twelves)
+  
+// } )
+
+// d20.addEventListener('click', () =>{
+//   const random = getRandomNumber(20)
+//   d20.src = `./images/numbers/${random}.png`
+//   twenties.push(random)
+//   document.querySelector("#d20-rolls-mean").innerText = findMean(twenties)
+//   document.querySelector("#d20-rolls-median").innerText = findMedian(twenties)
+//   document.querySelector("#d20-rolls-mode").innerText = findMode(twenties)
+  
+// } )
+
+
+
+// /******************
+//  * RESET FUNCTION *
+//  ******************/
+// document.querySelector('#reset-button').addEventListener('click', reset)
+
+// function reset(){
+//   console.log('called')
+//   d6.src = `./images/start/d6.png`
+//   doubleD6.forEach((die)=> die.src = `./images/start/d6.png` )
+//   d12.src = `./images/start/d12.png`
+//   d20.src = `./images/start/d20.png`
+
+//   while(sixes.length > 0){
+//     sixes.pop()
+  
+//   }
+//   while(doubleSixes.length > 0){
+//     doubleSixes.pop()
+  
+//   }
+//   while(twelves.length > 0){
+//     twelves.pop()
+  
+//   }
+//   while(twenties.length > 0){
+//     twenties.pop()
+  
+//   }
+
+//  document.querySelector('#d6-rolls-mean').innerText = 'NA'
+//  document.querySelector('#d6-rolls-median').innerText = 'NA'
+//  document.querySelector('#d6-rolls-mode').innerText = 'NA'
+//  document.querySelector('#double-d6-rolls-mean').innerText = 'NA'
+//  document.querySelector('#double-d6-rolls-meadian').innerText = 'NA'
+//  document.querySelector('#double-d6-rolls-mode').innerText = 'NA'
+//  document.querySelector('#d12-rolls-mean').innerText = 'NA'
+//  document.querySelector('#d12-rolls-median').innerText = 'NA'
+//  document.querySelector('#d12-rolls-mode').innerText = 'NA'
+//  document.querySelector('#d20-rolls-mean').innerText = 'NA'
+//  document.querySelector('#d20-rolls-median').innerText = 'NA'
+//  document.querySelector('#d20-rolls-mode').innerText = 'NA'
+
+// }
+
+
+
+// /****************************
+//  * CLICK HANDLING FUNCTIONS *
+// ****************************/
+
+
+
+// /****************
+//  * MATH SECTION *
+//  ****************/
+
+// function findMean(diceArr){
+//   let sum = 0;
+//   for(let rolls of diceArr ){
+//     sum += rolls
+//   }
+// return sum / diceArr.length
+// }
+
+// function findMedian(diceArr){
+//   let sortedArr = sortByNumber(diceArr.slice())
+//   if(diceArr.length % 2 !== 0){
+//     return sortedArr[Math.floor(diceArr.length / 2)]
+//   }else {
+//     let firstMedian = sortedArr[(diceArr.length / 2)-1]
+//     let secondMedian = sortedArr[(diceArr.length / 2)]
+//     return (firstMedian + secondMedian) / 2
+//   }
+// }
+
+// // function findMode(diceArr){
+// //   let countObj = {};
+// //   for(let rolls of diceArr){
+// //     if(countObj[rolls]){
+// //       countObj[rolls]++
+// //     }else {
+// //       countObj[rolls] = 1; 
+// //     }
+// //   }
+// // let mode = [];
+// // let mostRolls = 0;
+
+// // for(let key in countObj){
+// //   if(countObj[key] > mostRolls){
+// //     mostRolls = countObj[key];
+// //     mode = [key]
+// //   }else if(countObj[key] === mostRolls){
+// //     mode.push(key)
+// //   }
+// // }
+// //   console.log(countObj, mostRolls)
+// //   return mode
+
+// // }
+// reset()
+
+
 /**********
  * DATA *
  **********/
@@ -30,40 +228,41 @@ const sortByNumber = function(arr) {
  * YOUR CODE BELOW *
  *******************/
 const d6 = document.querySelector('#d6-roll');
+
 const doubleD6 = document.querySelectorAll('.double.d6.roll');
 const d12 = document.querySelector('#d12-roll');
 const d20 = document.querySelector('#d20-roll');
 
 
-
 /*******************
  * EVENT LISTENERS *
  *******************/
-d6.addEventListener('click',function(){
-  let counter = 0;
+d6.addEventListener('click', ()=>{
+  let counter = 0
   let random;
   const num = setInterval(() => {
-  random = getRandomNumber(6); 
-  d6.src = `./images/d6/${random}.png`;
-  counter++
-  if(counter >= 10){
-    clearInterval(num)
-    sixes.push(random)
-    document.getElementById('d6-rolls-mean').innerText = findMean(sixes)
-    document.getElementById('d6-rolls-median').innerText = findMedian(sixes)
-    document.getElementById('d6-rolls-mode').innerText = findMode(sixes)
-  }
-  },30)
+      random = getRandomNumber(6)
+      d6.src = `./images/d6/${random}.png`
+      counter++
+      if (counter >= 10) {
+        clearInterval(num)
+        sixes.push(random)
+        document.querySelector('#d6-rolls-mean').innerText = findMean(sixes)
+        document.querySelector('#d6-rolls-median').innerText = findMedian(sixes)
+        document.querySelector('#d6-rolls-mode').innerText = findMode(sixes)
+      }
+    }, 30)
+  
 })
 
-const roll6s = () => {
-  let roll1 = getRandomNumber(6);
-  let roll2 = getRandomNumber(6);
-  let roll = roll1 + roll2;
+const roll6s = () =>{
+  let roll1 = getRandomNumber(6)
+  let roll2 = getRandomNumber(6)
+  let roll = roll1 + roll2
   doubleSixes.push(roll)
-  document.querySelector("#double-d6-rolls-mean").innerText = findMean(doubleSixes)
-  document.querySelector("#double-d6-rolls-mode").innerText = findMode(doubleSixes)
-  document.querySelector("#double-d6-rolls-median").innerText = findMedian(doubleSixes)
+  document.querySelector('#double-d6-rolls-mean').innerText = findMean(doubleSixes)
+  document.querySelector('#double-d6-rolls-median').innerText = findMedian(doubleSixes)
+  document.querySelector('#double-d6-rolls-mode').innerText = findMode(doubleSixes)
   doubleD6[0].src = `./images/d6/${roll1}.png`
   doubleD6[1].src = `./images/d6/${roll2}.png`
 }
@@ -72,25 +271,24 @@ doubleD6.forEach((die)=>{
   die.addEventListener('click', roll6s)
 })
 
-d12.addEventListener('click', () =>{
+
+d12.addEventListener('click', ()=>{
   const random = getRandomNumber(12)
   d12.src = `./images/numbers/${random}.png`
   twelves.push(random)
-  document.querySelector("#d12-rolls-mean").innerText = findMean(twelves)
-  document.querySelector("#d12-rolls-median").innerText = findMedian(twelves)
-  document.querySelector("#d12-rolls-mode").innerText = findMode(twelves)
-  
-} )
+  document.querySelector('#d12-rolls-mean').innerText = findMean(twelves)
+  document.querySelector('#d12-rolls-median').innerText = findMedian(twelves)
+  document.querySelector('#d12-rolls-mode').innerText = findMode(twelves)
+})
 
-d20.addEventListener('click', () =>{
+d20.addEventListener('click', ()=>{
   const random = getRandomNumber(20)
   d20.src = `./images/numbers/${random}.png`
   twenties.push(random)
-  document.querySelector("#d20-rolls-mean").innerText = findMean(twenties)
-  document.querySelector("#d20-rolls-median").innerText = findMedian(twenties)
-  document.querySelector("#d20-rolls-mode").innerText = findMode(twenties)
-  
-} )
+  document.querySelector('#d20-rolls-mean').innerText = findMean(twenties)
+  document.querySelector('#d20-rolls-median').innerText = findMedian(twenties)
+  document.querySelector('#d20-rolls-mode').innerText = findMode(twenties)
+})
 
 
 
@@ -101,45 +299,37 @@ document.querySelector('#reset-button').addEventListener('click', reset)
 
 function reset(){
   console.log('called')
-  d6.src = `./images/start/d6.png`
-  doubleD6.forEach((die)=> die.src = `./images/start/d6.png` )
-  d12.src = `./images/start/d12.png`
-  d20.src = `./images/start/d20.png`
-
-  while(sixes.length > 0){
-    sixes.pop()
-  
+    d6.src = './images/start/d6.png'
+    doubleD6.forEach((die)=>die.src = './images/start/d6.png')
+    d12.src = './images/start/d12.jpeg'
+    d20.src = './images/start/d20.jpg'
+    while(sixes.length > 0){
+      sixes.pop()
+    }
+    while(doubleSixes.length > 0){
+      doubleSixes.pop()
+    }
+    while(twelves.length > 0){
+      twelves.pop()
+    }
+    while(twenties.length > 0){
+      twenties.pop()
+    }
+    document.querySelector('#d6-rolls-mean').innerText = 'NA'
+    document.querySelector('#d6-rolls-median').innerText = 'NA'
+    document.querySelector('#d6-rolls-mode').innerText = 'NA'
+    document.querySelector('#double-d6-rolls-mean').innerText = 'NA'
+    document.querySelector('#double-d6-rolls-median').innerText = 'NA'
+    document.querySelector('#double-d6-rolls-mode').innerText = 'NA'
+    document.querySelector('#d12-rolls-mean').innerText = 'NA'
+    document.querySelector('#d12-rolls-median').innerText = 'NA'
+    document.querySelector('#d12-rolls-mode').innerText = 'NA'
+    document.querySelector('#d20-rolls-mean').innerText = 'NA'
+    document.querySelector('#d20-rolls-median').innerText = 'NA'
+    document.querySelector('#d20-rolls-mode').innerText = 'NA'
   }
-  while(doubleSixes.length > 0){
-    doubleSixes.pop()
   
-  }
-  while(twelves.length > 0){
-    twelves.pop()
   
-  }
-  while(twenties.length > 0){
-    twenties.pop()
-  
-  }
-
- document.querySelector('#d6-rolls-mean').innerText = 'NA'
- document.querySelector('#d6-rolls-median').innerText = 'NA'
- document.querySelector('#d6-rolls-mode').innerText = 'NA'
- document.querySelector('#double-d6-rolls-mean').innerText = 'NA'
- document.querySelector('#double-d6-rolls-meadian').innerText = 'NA'
- document.querySelector('#double-d6-rolls-mode').innerText = 'NA'
- document.querySelector('#d12-rolls-mean').innerText = 'NA'
- document.querySelector('#d12-rolls-median').innerText = 'NA'
- document.querySelector('#d12-rolls-mode').innerText = 'NA'
- document.querySelector('#d20-rolls-mean').innerText = 'NA'
- document.querySelector('#d20-rolls-median').innerText = 'NA'
- document.querySelector('#d20-rolls-mode').innerText = 'NA'
-
-}
-
-
-
 /****************************
  * CLICK HANDLING FUNCTIONS *
 ****************************/
@@ -149,4 +339,51 @@ function reset(){
 /****************
  * MATH SECTION *
  ****************/
+function findMean(diceArr){
+  let sum = 0;
+  for(let roll of diceArr){
+    sum += roll
+  }
+  return sum / diceArr.length
+}
+
+function findMedian(diceArr){
+  sortedArr = sortByNumber(diceArr.slice())
+  if(diceArr.length % 2 !== 0){
+    return sortedArr[Math.floor(diceArr.length / 2)]
+  }else{
+    let firstMedian = sortedArr[(diceArr.length / 2) - 1]
+    let secondMedian = sortedArr[(diceArr.length/2)]
+    return (firstMedian + secondMedian) / 2
+  }
+}
+function findMode(diceArr){
+  let countObj = {};
+  for(let roll of diceArr){
+    if(countObj[roll]){
+      countObj[roll]++
+    }else{
+      countObj[roll] = 1;
+    }
+  }
+
+  let mode = [];
+  let mostRolls = 0;
+  for(let key in countObj){
+    if(countObj[key] > mostRolls){
+      mostRolls = countObj[key];
+      mode = [key];
+    }else if(countObj[key] === mostRolls){
+      mode.push(key)
+    }
+  }
+  console.log(countObj, mostRolls)
+  return mode
+}
+reset();
+
+
+
+
+
 
